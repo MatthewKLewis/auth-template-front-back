3149e8043962d7d933613e11f2bb82eb7b3aa429
Matthew Lewis <matthew.knight.lewis@gmail.com>
2021-05-01 15:52:14 -0400

    working in dependencies

--- backend/server.js
+++ backend/server.js
@@ -1,61 +1,31 @@
-//Importing...
-const express = require("express");
-const path = require('path');
+//Imports
+const express = require("express")
+const path = require('path')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const passport = require('passport-jwt')
 const dotenv = require('dotenv')
 
+//Models
+const User = require("./user.model")
+
+//Routes
+const userRoutes = require('./routes/users')
+
 //Configuring...
-const User = require("./user.model");
+const port = process.env.PORT || 4000
 const app = express();
-app.use(express.json());
+app.use(bodyParser.json())
+app.use('/users', userRoutes)
 
-app.set("json spaces", 2);
+//Set Static Folder
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('/', (req, res) => {
-  res.redirect('/documentation')
+app.get('/', (req, res)=>{
+  res.send('Invalid Endpoint')
 })
 
-app.get("/documentation", (req, res) => {
-  res.sendFile(path.join(__dirname + '/documentation.html'), (err)=> {
-    if (err) console.log(err);
-  });
-});
 
-app.get("/user/:id", (req, res) => {
-  User.findById({ _id: req.params.id }).then((User) => {
-    res.json(User);
-  });
-});
-
-app.post("/register", (req, res) => {
-  var newUser = new User({
-    "Name": req.body.name,
-    "Password": "1245",
-  })
-
-  newUser.save((err) => {
-    if (err) throw err;
-    else res.send('User Created');
-  });
-});
-
-app.put("/user/update/:id", (req, res) => {
-  User.findById({ _id: req.params.id }).then((User) => {
-    User["Official Name"] = req.body.name
-    User.save((err) => {
-      if (err) throw err;
-      else res.send('User Created');
-    });
-  });
-});
-
-app.delete("/user/delete/:id", (req, res) => {
-  User.findByIdAndDelete({ _id: req.params.id }).then(() => {
-    res.send("User Deleted");
-  });
-});
-
-app.set('port', process.env.PORT || 4000)
-
-app.listen(app.get('port'), () => {
-  console.log(`Listening on Port: ${app.get('port')}`);
+app.listen(port, () => {
+  console.log(`Listening on Port: ${port}`);
 });
