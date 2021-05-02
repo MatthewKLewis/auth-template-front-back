61af1fd860e238f41d384e95d42502ed8a064315
Matthew Lewis <matthew.knight.lewis@gmail.com>
2021-05-02 13:26:01 -0400

    validated form to register complete

--- angular-src/src/app/register/register.component.ts
+++ angular-src/src/app/register/register.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import {  FormBuilder,  FormControl,  FormGroup,  Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { UserService } from '../services/user.service';
 
 @Component({
@@ -10,33 +15,59 @@ import { UserService } from '../services/user.service';
 export class RegisterComponent implements OnInit {
   registerForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder, private userService: UserService) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private userService: UserService
+  ) {
+    this.registerForm = this.formBuilder.group(
+      {
+        username: new FormControl('', [
+          Validators.required,
+          Validators.minLength(4),
+        ]),
+        email: new FormControl('', [
+          Validators.required,
+          Validators.minLength(4),
+          Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$'),
+        ]),
+        password: new FormControl('', [
+          Validators.required,
+          Validators.minLength(4),
+        ]),
+        password2: new FormControl('', [
+          Validators.required,
+          Validators.minLength(4),
+        ]),
+      },
+      { validator: passwordMatchValidator }
+    );
 
-    //"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
-    this.registerForm = this.formBuilder.group({
-      username: new FormControl('', [
-        Validators.required,
-        Validators.minLength(4),
-      ]),
-      email: new FormControl('', [
-        Validators.required,
-        Validators.minLength(4),
-        Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')
-      ]),
-      password: new FormControl('', [
-        Validators.required,
-        Validators.minLength(4),
-      ]),
-      password2: new FormControl('', [
-        Validators.required,
-        Validators.minLength(4),
-      ]),
-    });
+    function passwordMatchValidator(g: FormGroup) {
+      return g.get('password')?.value === g.get('password2')?.value
+        ? null
+        : { mismatch: true };
+    }
   }
 
   ngOnInit(): void {}
 
+  get username() {
+    return this.registerForm.get('username');
+  }
+
+  get email() {
+    return this.registerForm.get('email');
+  }
+
+  get password() {
+    return this.registerForm.get('password');
+  }
+
+  get password2() {
+    return this.registerForm.get('password2');
+  }
+
   onSubmit() {
-    console.log(this.registerForm.value);
+    console.log(this.registerForm);
   }
 }
